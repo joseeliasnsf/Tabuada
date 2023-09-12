f7997454919f6b1adef382aa3173f870d4577c81
Jose Elias Nunes Silva Filho <jensf_dev@proton.me>
2023-09-12 06:58:22 -0300

    tabuada
    Upload

---
+++ tabuada.py
@@ -0,0 +1,12 @@
+n1 = int(input('Digite um numero para saber sua tabuada: '))
+n2 = n1
+n3 = n1 * 2
+n4 = n1 * 3
+n5 = n1 * 4
+n6 = n1 * 5
+n7 = n1 * 6
+n8 = n1 * 7
+n9 = n1 * 8
+n10 = n1 * 9
+n11 = n1 * 10
+print('A tabuada de: {} é: \n2 * 2: {} \n2 * 3: {} \n2 * 4: {} \n2 * 5: {} \n2 * 6: {} \n2 * 7: {} \n2 * 8: {} \n2 * 9: {} \n2 * 10: {}'.format(n2,n3,n4,n5,n6,n7,n8,n9,n10,n11))
